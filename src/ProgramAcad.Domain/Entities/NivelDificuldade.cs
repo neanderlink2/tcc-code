bb8828898e248b2bf86dba16ce1fb0beaa875910
Neander <42013621+neanderlink2@users.noreply.github.com>
2019-11-04 11:38:15 -0300

    Adicionado novo campo em NivelDificuldade

--- src/ProgramAcad.Domain/Entities/NivelDificuldade.cs
+++ src/ProgramAcad.Domain/Entities/NivelDificuldade.cs
@@ -1,19 +1,21 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace ProgramAcad.Domain.Entities
 {
     public class NivelDificuldade
     {
-        public NivelDificuldade(int id, int nivel, string descricao)
+        public NivelDificuldade(int id, int nivel, string descricao, int pontosReceber)
         {
             Id = id;
             Nivel = nivel;
             Descricao = descricao;
+            PontosReceber = pontosReceber;
         }
 
         public int Id { get; protected set; }
         public int Nivel { get; protected set; }
         public string Descricao { get; protected set; }
+        public int PontosReceber { get; protected set; }
 
         public ICollection<Algoritmo> AlgoritmosDesseNivel { get; set; }
         public ICollection<ExecucaoTeste> TestesExecutados { get; set; }
